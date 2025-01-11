343a3cd39cb61f15d64d8dd7c62a12130b77f78d
LamPaturle-AI <lam.paturle@gmail.com>
2025-01-11 20:09:41 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,31 @@
+# Change directory to project directory
+cd /Users/lampaturle/Desktop/SWITCH_PROJECT/Datascientest/examen-dvc/
+
+# Create new virtual environnement
+mamba create -n dsdvcexam python=3.9
+
+# Activate virtual environnement
+mamba activate dsdvcexam
+
+# Install dvc
+pip install dvc
+
+# Initialize dvc
+dvc init -f
+
+# Install AWS s3
+pip install "dvc[s3]"
+
+# Add a DagsHub DVC remote
+dvc remote add origin s3://dvc
+dvc remote modify origin endpointurl https://dagshub.com/LamPaturle-AI/examen-dvc.s3
+
+# Setup credentials
+dvc remote modify origin --local access_key_id 8a08ba4d3a9988a7f9c1664a9c97b2354641694d
+dvc remote modify origin --local secret_access_key 8a08ba4d3a9988a7f9c1664a9c97b2354641694d
+
+# Setup default origin
+dvc remote default origin
+
+# Save
+dvc push
