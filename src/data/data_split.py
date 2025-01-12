521d8ed36af48858ce8a64897832b74d6f9df103
LamPaturle-AI <lam.paturle@gmail.com>
2025-01-12 16:00:14 +0100

    Update data_split.py

--- src/data/data_split.py
+++ src/data/data_split.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 # Read the raw CSV
-df = pd.read_csv("data/raw_data/raw.csv")
+df = pd.read_csv("data/raw/raw.csv")
 
 # Separate features (X) and target (y)
 #    - Drop 'silica_concentrate' from X
@@ -19,9 +19,9 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 # Write the resulting splits to CSV
-X_train.to_csv("data/processed_data/X_train.csv", index=False)
-X_test.to_csv("data/processed_data/X_test.csv", index=False)
-y_train.to_csv("data/processed_data/y_train.csv", index=False)
-y_test.to_csv("data/processed_data/y_test.csv", index=False)
+X_train.to_csv("data/processed/X_train.csv", index=False)
+X_test.to_csv("data/processed/X_test.csv", index=False)
+y_train.to_csv("data/processed/y_train.csv", index=False)
+y_test.to_csv("data/processed/y_test.csv", index=False)
 
-print("4 datasets X_train.csv, X_test.csv, y_train.csv and y_test.csv saved to data/processed_data.")
+print("4 datasets X_train.csv, X_test.csv, y_train.csv and y_test.csv saved to data/processed.")
