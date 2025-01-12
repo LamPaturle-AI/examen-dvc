b5e9a1e6a86d8f2df51e63e4a662e76a7583858e
LamPaturle-AI <lam.paturle@gmail.com>
2025-01-12 22:03:35 +0100

    Update training.py

--- src/models/training.py
+++ src/models/training.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn.ensemble import GradientBoostingRegressor
 import joblib
 
-# Load the training data
+# Load training data
 X_train = pd.read_csv("data/processed/X_train_scaled.csv")
 y_train = pd.read_csv("data/processed/y_train.csv")
 
@@ -11,16 +11,16 @@ y_train = pd.read_csv("data/processed/y_train.csv")
 params_path = "models/best_params.pkl"
 best_params = joblib.load(params_path)
 
-# Create a GradientBoostingRegressor with those best parameters
+# Create GradientBoostingRegressor with best parameters
 gbr = GradientBoostingRegressor(random_state=42, **best_params)
 
-# Fit the model on the training data
+# Fit model on training data
 gbr.fit(X_train, y_train)
 
-# Make sure the models folder exists
+# Make sure models folder exists
 os.makedirs("models", exist_ok=True)
 
-# Save the trained model
+# Save trained model
 joblib.dump(gbr, "models/gbr_model.pkl")
 
 print("Trained Gradient Boosting model saved to models.")
