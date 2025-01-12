30db17c4bc0925ad47b7ec45d367858451856db3
LamPaturle-AI <lam.paturle@gmail.com>
2025-01-12 22:03:32 +0100

    Update evaluate.py

--- src/data/evaluate.py
+++ src/data/evaluate.py
@@ -4,21 +4,21 @@ from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 import json
 import joblib
 
-# Load the trained model
+# Load trained model
 model_path = "models/gbr_model.pkl"
 model = joblib.load(model_path)
 
-# Load the test data
+# Load test data
 X_test = pd.read_csv("data/processed/X_test_scaled.csv")
 y_test = pd.read_csv("data/processed/y_test.csv")
 
 # Make predictions
 y_pred = model.predict(X_test)
 
-# Make sure the 'data' folder exists
-os.makedirs("data", exist_ok=True)
+# Make sure 'data/processed' folder exists
+os.makedirs("data/processed", exist_ok=True)
 
-# Save predictions to CSV
+# Write predictions to CSV
 prediction_df = pd.DataFrame({"prediction": y_pred})
 prediction_csv_path = "data/processed/prediction.csv"
 prediction_df.to_csv(prediction_csv_path, index=False)
@@ -29,7 +29,7 @@ mse = mean_squared_error(y_test,y_pred)
 mae = mean_absolute_error(y_test,y_pred)
 r2 = r2_score(y_test, y_pred)
 
-# Make sure the metrics folder exists
+# Make sure metrics folder exists
 os.makedirs("metrics", exist_ok=True)
 
 # Save metrics to JSON
